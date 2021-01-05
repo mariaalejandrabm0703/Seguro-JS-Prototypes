0eb07851ba4ccd6c5c9d349516d21420e3d84bc4
Your Name <you@example.com>
2021-01-05 10:09:49 -0500

    Seleccionar los valores del form.

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ function insurance(brand, year, type) {
   this.type = type;
 }
 
-function UI() {}
+function interface() {}
 
 // Llenar los años
-UI.prototype.SetYears = () => {
+interface.prototype.SetYears = () => {
   const max = new Date().getFullYear();
   min = max - 20;
 
@@ -21,8 +21,33 @@ UI.prototype.SetYears = () => {
   }
 };
 
-const ui = new UI();
+const ui = new interface();
 
 document.addEventListener("DOMContentLoaded", () => {
   ui.SetYears();
 });
+
+eventListener();
+
+function eventListener() {
+  const form = document.querySelector("#cotizar-seguro");
+  form.addEventListener("submit", quoteInsurance);
+}
+
+function quoteInsurance(e) {
+  e.preventDefault();
+
+  const brand = document.querySelector("#marca").value;
+
+  const year = document.querySelector("#year").value;
+
+  const type = document.querySelector('input[name="tipo"]:checked').value;
+
+  if (brand === "") {
+    console.log("no marca");
+  } else {
+    console.log("si valores");
+  }
+
+  console.log("brand " + brand + "year " + year + "type " + type);
+}
